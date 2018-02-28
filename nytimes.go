3c49125b50dc44fca7ae53e89048bcf121aa1ae7
Jaharsh Samayan <jaharsh93@gmail.com>
2018-02-28 14:57:00 -0700

    Update nytimes.go

--- nytimes.go
+++ nytimes.go
@@ -1,3 +1,5 @@
+//curl -X POST -H "Content-Type: application/octet-stream" --data-binary '@words.txt' http://127.0.0.1:5050/upload
+
 package main
 
 import (
@@ -13,6 +15,12 @@ var m map[string]string =make(map[string]string)
 var al []string 
 var t = trie.New() 
 
+type Health_Param struct {
+    param_name        string   
+    param_status      string   
+    
+}
+
 func build_dictionary(ip string){
     
     sp:= strings.Split(ip,"\n")
@@ -26,32 +34,24 @@ func build_dictionary(ip string){
 
 
 func display_dictionary(ip string){
-    
-     
-    var tem string
-    
+    var tem string    
     ip=ip+"#"
     var flg int
-    for itr:=0;itr<len(ip)-1;itr=itr+1{
-        
-      ch:=ip[itr]
+    for itr:=0;itr<len(ip)-1;itr=itr+1{       
+        ch:=ip[itr]
         tem=tem+string(ch)
-        
         if t.HasChildren(tem,rune(ip[itr+1])){
             if _, ok := t.Find(tem);ok{
-            //fmt.Println(tem)
             flg=itr
             }
         }else{
-            
             if _, ok := t.Find(tem);ok{
-                 //fmt.Println(tem)
             flg=itr
             }
             tem=tem[0:(len(tem)-(itr-flg))]
             fmt.Println(tem)
-          tem=""
-         itr = flg  
+            tem=""
+            itr = flg  
         }
         
     }
@@ -59,10 +59,54 @@ func display_dictionary(ip string){
 }
 
 
-
-func display_dictionary2(ip string){
+func walk_display_dictionary(ip string){
+    var tem string    
+    ip=ip+"#"
+    var flg int
+    for itr:=0;itr<len(ip)-1;itr=itr+1{       
+        ch:=ip[itr]
+        tem=tem+string(ch)
+        if t.HasChildren(tem,rune(ip[itr+1])){
+            if _, ok := t.Find(tem);ok{
+            fmt.Println(tem)
+            flg=itr
+            }
+        }else{
+            if _, ok := t.Find(tem);ok{
+            fmt.Println(tem)
+            flg=itr
+            }
+            tem=""
+            itr = flg  
+        }
+        
+    }
     
-     
+}
+
+func backtrack_display_dictionary(ip string){
+    tem:=""
+    ip=ip+"#"
+    for i:=0;i<len(ip);i=i+1{
+        tem=tem+string(ip[i])
+        for j:=i+1;j<len(ip);j=j+1{
+            if t.HasChildren(tem,rune(ip[j])){
+            tem=ip[i:j+1]
+            if _, ok := t.Find(tem);ok{
+                fmt.Println(tem)
+            }
+            }else{
+                tem=""
+                break
+            }
+            
+        }
+    }
+    
+}
+
+
+func brute_force_display_dictionary(ip string){
     var tem string
     for i:=0;i<len(ip);i=i+1{
         for j:=i;j<len(ip);j=j+1{
@@ -78,10 +122,6 @@ func display_dictionary2(ip string){
 
 
 
-
-
-
-
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	
     var s string
@@ -93,13 +133,10 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
     }
     s = string(bs)
     build_dictionary(s)
-   
-	
-	
 }
 
 
-func uploadHandler2(w http.ResponseWriter, r *http.Request) {
+func displayHandler(w http.ResponseWriter, r *http.Request) {
 	
     var s string
     bs,er := ioutil.ReadAll(r.Body)
@@ -109,15 +146,34 @@ func uploadHandler2(w http.ResponseWriter, r *http.Request) {
     return
     }
     s = string(bs)
-    display_dictionary(s)
-   
-	
-	
+    backtrack_display_dictionary(s)
 }
 
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	
+//    //simple_check:=Health_Param{"status","OK"}
+//    jData, err := json.MarshalIndent(simple_check,"","  ")
+//if err != nil {
+//    panic(err)
+//    return
+//}
+//    fmt.Println(string(jData))
+//w.Header().Set("Content-Type", "application/json")
+    var jsonBlob = []byte(`{"status": "OK"}`)
+//    w.Header().Set("Content-Type", "application/json")
+//    json.NewEncoder(w).Encode(jsonBlob)
+    
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonBlob)
+	
+}
+ 
+
 func main() {
-	http.HandleFunc("/upload", uploadHandler)
-    http.HandleFunc("/display", uploadHandler2)
+	http.HandleFunc("/dictionary", uploadHandler)
+    http.HandleFunc("/split", displayHandler)
+    http.HandleFunc("/healthcheck", healthcheck)
 	http.ListenAndServe(":5050", nil)
 }
 
